ea33b2ddcb0c150a270241ed20f39509d21f4a4d
Emiliano <emiliano.masi@fiz-karlsruhe.de>
2014-01-10 19:10:33 +0100

    Rejecting Rogue Cross Origin Request

--- api/controllers/ApiController.php
+++ api/controllers/ApiController.php
@@ -5,15 +5,25 @@ include_once (realpath(__DIR__ . '/../models/TwitterModel.php'));
 class ApiController {
 	public $model;
 	private $constants = array(module => 'module', method => 'method');
+	private $allowedOrigins = array("http://www.emilianomasi.com","http://emilianomasi.com");
 	public function __construct() {}
+	
+	private function checkCORs(){
+		if(in_array($_SERVER['HTTP_ORIGIN'], $this->allowedOrigins)){
+			header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+		}
+		else{
+			$error = array(status => '401', message => "Origin '".$_SERVER['HTTP_ORIGIN']."' is not in the list, and is therefore not allowed access.");
+			echo json_encode($error);
+			exit();
+		}
+	}
 	
 	public function invoke() {
 		header('Content-type: application/json');
-		header('Access-Control-Allow-Origin: http://www.emilianomasi.com');
-		header('Access-Control-Allow-Origin: http://emilianomasi.com');
+		$this->checkCORs();
 		if (isset ( $_GET [$this->constants[module]] )) {
 			if($_GET [$this->constants[module]] == "twitter"){
-				
 				if($_GET [$this->constants[method]] == "tweets"){
 					$this->model = new TwitterModel();
 					$tweets = $this->model->getTweets($_GET);
